b7700f986e84ef73c450bfe7a4384a85d405ed22
Sahil Chowdhury <sahils@Shafins-MacBook-Pro.local>
2023-06-04 12:13:14 -0500

    flask practice

--- day55/higherlower.py
+++ day55/higherlower.py
@@ -3,18 +3,27 @@ import random
 
 app = Flask(__name__)
 
-randomnumber = random.randint(1,10)
+random = random.randint(1,10)
 
 @app.route("/")
 def home():
     return "<h1>Guess a number from 1 to 10</h1>" \
-           '<img src="https://media.giphy.com/media/qVDmGRCLEX4Z2FpyUX/giphy.gif">'
+           "<img src='https://media.giphy.com/media/qVDmGRCLEX4Z2FpyUX/giphy.gif'>"
 
-
-@app.route("/<guess>")
+@app.route("/<int:guess>")
 def guess_number(guess):
-    if guess > randomnumber:
-        "<h1 "
+    if guess < random:
+        return "<h1 style='color:red'>Too low!</h1>" \
+               "<img src='https://media.giphy.com/media/l4KibK3JwaVo0CjDO/giphy.gif'>"
+    elif guess > random:
+        return "<h1 style='color:high'>Too high!</h1>" \
+               "<img src='https://media.giphy.com/media/YNPjwaQdIb3K8/giphy.gif'>"
+    else:
+        return "<h1>CORRECT!!</h1>" \
+               "<img src='https://media.giphy.com/media/QvBoMEcQ7DQXK/giphy.gif'>"
+    
+
+
 
 
 if __name__ == "__main__":
