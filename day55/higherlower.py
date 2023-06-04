cfbf9dcb4eedfb705c2651e41ed74de30ef4d82f
Sahil Chowdhury <sahilschowdhury@gmail.com>
2023-06-04 12:13:59 -0500

    flask stuff

--- day55/higherlower.py
+++ day55/higherlower.py
@@ -7,11 +7,14 @@ randomnumber = random.randint(1,10)
 
 @app.route("/")
 def home():
-    return "<h1>Guess a number from 1 to 10"
+    return "<h1>Guess a number from 1 to 10</h1>" \
+           '<img src="https://media.giphy.com/media/qVDmGRCLEX4Z2FpyUX/giphy.gif">'
+
 
 @app.route("/<guess>")
 def guess_number(guess):
-    if
+    if guess > randomnumber:
+        "<h1 "
 
 
 if __name__ == "__main__":
